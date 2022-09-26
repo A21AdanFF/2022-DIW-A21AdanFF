e5f491c828b1069f245bd820fa2596cad131fc20
A21AdanFF <A21AdanFF@iessanclemente.net>
2022-09-26 11:16:34 +0200

    New styles

---
+++ bakery/src/components/Product.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let title;
+    export let img;
+    export let id;
+</script>
+
+<a href="/{id}" class="flex w-full max-w-4xl justify-center">
+    <div class="h-40 w-40 flex items-end justify-center rounded-sm hover:shadow-md transition-all duration-100" style="background:{img}">
+        <span class="w-full py-2 bg-bakery-light flex justify-center cursor-pointer">{title}</span>
+    </div>
+</a>
--- bakery/tailwind.config.cjs
+++ bakery/tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        bakery: {
+          primary: "#ccb096",
+          light: "#deccba",
+          dark: "#7d664f",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
