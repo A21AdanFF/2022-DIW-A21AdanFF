baaa33f43e7e813f303e777e768a06c441c3c791
A21AdanFF <A21AdanFF@iessanclemente.net>
2022-09-27 12:15:36 +0200

    Fixes

--- bakery/src/components/Product.svelte
+++ bakery/src/components/Product.svelte
@@ -4,8 +4,8 @@
     export let id;
 </script>
 
-<a href="/{id}" class="flex w-full max-w-4xl justify-center">
-    <div class="h-40 w-40 flex items-end justify-center rounded-sm hover:shadow-md transition-all duration-100" style="background:{img}">
-        <span class="w-full py-2 bg-bakery-light flex justify-center cursor-pointer">{title}</span>
+<a href="/{id}" class="w-full max-w-4xl justify-center grid gap-2 hover:shadow-md py-2 transition-all duration-100">
+    <div class="h-40 w-40 flex items-end justify-center rounded-sm" style="background:{img}">
     </div>
+    <span class="w-full flex justify-center cursor-pointer">{title}</span>
 </a>
